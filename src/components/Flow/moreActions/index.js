6538a908150c44b5379c271733bd367167bca39a
Jason Skomorowski <jason@indexdata.com>
2026-04-16 15:27:55 -0400

    Remove old mod-rs actions/modals/translations ILLDEV-274

--- src/components/Flow/moreActions/index.js
+++ src/components/Flow/moreActions/index.js
@@ -1,27 +1,8 @@
 import React from 'react';
-import { FormattedMessage } from 'react-intl';
-import { DirectLink } from '@projectreshare/stripes-reshare';
-import { Button, Icon } from '@folio/stripes/components';
-import { IfInterface } from '@folio/stripes/core';
-import { ShowModalButton } from '../../ModalButtons';
 import ActionReasonButton from '../ActionReasonButton';
 import { ReasonUnfilled } from '../../../constants/iso18626';
 
 export { default as Generic } from './Generic';
-export { default as AddManualFee } from './AddManualFee';
-
-// Named corresponding to actions
-export const CancelLocal = () => (
-  <ShowModalButton buttonStyle="dropdownItem" modal="cancelLocal">
-    <Icon icon="times-circle-solid"><FormattedMessage id="ui-rs.actions.requesterCancel" /></Icon>
-  </ShowModalButton>
-);
-
-export const ManualClose = () => (
-  <ShowModalButton buttonStyle="dropdownItem" modal="ManualClose">
-    <Icon icon="exclamation-circle"><FormattedMessage id="ui-rs.actions.manualClose" /></Icon>
-  </ShowModalButton>
-);
 
 export const CannotSupply = props => (
   <ActionReasonButton
@@ -31,73 +12,4 @@ export const CannotSupply = props => (
     reasonTranslationPrefix="ui-rs.iso18626.ReasonUnfilled"
     {...props}
   />
-);
-
-export const NonreturnableSupplierRespondCannotSupply = () => (
-  <ShowModalButton buttonStyle="dropdownItem" modal="nonreturnableSupplierRespondCannotSupply">
-    <Icon icon="times-circle-solid"><FormattedMessage id="ui-rs.actions.nonreturnableSupplierRespondCannotSupply" /></Icon>
-  </ShowModalButton>
-);
-
-export const SupplierConditionalSupply = () => (
-  <ShowModalButton buttonStyle="dropdownItem" modal="ConditionalSupply">
-    <Icon icon="ellipsis"><FormattedMessage id="ui-rs.actions.conditionalSupply" /></Icon>
-  </ShowModalButton>
-);
-
-export const SupplierAddCondition = () => (
-  <ShowModalButton buttonStyle="dropdownItem" modal="AddCondition">
-    <Icon icon="plus-sign"><FormattedMessage id="ui-rs.actions.addCondition" /></Icon>
-  </ShowModalButton>
-);
-
-export const Rerequest = () => (
-  <DirectLink component={Button} buttonStyle="dropdownItem" to="rerequest">
-    <Icon icon="refresh"><FormattedMessage id="stripes-reshare.actions.rerequest" /></Icon>
-  </DirectLink>
-);
-
-export const RequesterCancel = () => (
-  <ShowModalButton buttonStyle="dropdownItem" modal="requesterCancel">
-    <Icon icon="times-circle-solid"><FormattedMessage id="ui-rs.actions.requesterCancel" /></Icon>
-  </ShowModalButton>
-);
-
-export const RequesterRetryValidation = () => (
-  <DirectLink component={Button} buttonStyle="dropdownItem" to="revalidate">
-    <Icon icon="refresh"><FormattedMessage id="stripes-reshare.actions.requesterRetryValidation" /></Icon>
-  </DirectLink>
-);
-
-export const RespondYes = () => (
-  <ShowModalButton buttonStyle="dropdownItem" modal="RespondYes">
-    <Icon icon="check-circle"><FormattedMessage id="ui-rs.actions.respondYes" /></Icon>
-  </ShowModalButton>
-);
-
-// Client only, component names do not correspond to mod-rs actions
-export const PrintPullSlip = () => (
-  <DirectLink component={Button} buttonStyle="dropdownItem" to="pullslip">
-    <Icon icon="print"><FormattedMessage id="ui-rs.printPullslip" /></Icon>
-  </DirectLink>
-);
-
-export const FillMultiVolumeRequest = () => (
-  <ShowModalButton buttonStyle="dropdownItem" modal="FillMultiVolumeRequest">
-    <Icon icon="check-circle"><FormattedMessage id="ui-rs.actions.fillMultiVolumeRequest" /></Icon>
-  </ShowModalButton>
-);
-
-export const SlnpAbortSupply = () => (
-  <ShowModalButton buttonStyle="dropdownItem" modal="slnpAbortSupply">
-    <Icon icon="times-circle-solid"><FormattedMessage id="ui-rs.actions.slnpAbortSupply" /></Icon>
-  </ShowModalButton>
-);
-
-export const UploadToFill = () => (
-  <IfInterface name="dms">
-    <ShowModalButton buttonStyle="dropdownItem" modal="UploadToFill">
-      <Icon icon="arrow-up"><FormattedMessage id="ui-rs.actions.uploadToFill" /></Icon>
-    </ShowModalButton>
-  </IfInterface>
 );
--- src/components/ViewMessageBanners.js
+++ src/components/ViewMessageBanners.js
@@ -3,15 +3,10 @@ import { FormattedMessage } from 'react-intl';
 import PropTypes from 'prop-types';
 import { MessageBanner } from '@folio/stripes/components';
 import { useStripes } from '@folio/stripes/core';
-import useActionConfig from './Flow/useActionConfig';
 
 const ViewMessageBanners = ({ request }) => {
   const stripes = useStripes();
-  // eslint-disable-next-line camelcase
-  const { combine_fill_and_ship } = useActionConfig();
-  // eslint-disable-next-line camelcase
-  const combine = combine_fill_and_ship === 'yes';
-  const lastCostStates = ['RES_COPY_AWAIT_PICKING', combine ? 'RES_AWAIT_PICKING' : 'RES_AWAIT_SHIP'];
+  const lastCostStates = ['RES_COPY_AWAIT_PICKING', 'RES_AWAIT_SHIP'];
   const lastChanceForCost = stripes.config?.reshare?.useTiers && stripes.config?.reshare?.showCost && lastCostStates.includes(request?.state?.code);
 
   const currentSupplier = request.resolvedSupplier?.id ?? request.supplyingInstitutionSymbol;
